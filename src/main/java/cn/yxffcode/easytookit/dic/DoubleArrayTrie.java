aa1c6190c0c65501221118bd2b2a4f18b0f05348
hang.gao <hang.gao@limiku.com>
2015-12-13 22:33:11 +0800

    重命名

--- src/main/java/cn/yxffcode/easytookit/dic/DoubleArrayTrie.java
+++ src/main/java/cn/yxffcode/easytookit/dic/DoubleArrayTrie.java
@@ -53,7 +53,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * <p/>
  * 内存存储方面,需要对字母表做处理,{@link #alphabetTransformer}用作奖字符转换成int类型的数据(apply方法中可返回IntsRef的包装类),
  * 默认只处理英文大小写,数字和汉字,其它字符跳过(转换成int后小于或者等于0).默认的方式将终止符转换成1,数字转换成2~11,小写字母转换成12~37,大写英文字母
- * 转换成38~73,汉字大于74(汉字转换成int后数值满园在[{@link IntsRefWrapper#CN_CHAR_FIRST},{@link IntsRefWrapper#CN_CHAR_LAST}]
+ * 转换成38~73,汉字大于74(汉字转换成int后数值满园在[{@link DefaultAlphabetTransformer#CN_CHAR_FIRST},{@link DefaultAlphabetTransformer#CN_CHAR_LAST}]
  * 之间,做字母表的转换可减小字母的数值,从而减小base与check两个数组的大小.
  *
  * @author gaohang on 15/12/9.
@@ -74,7 +74,7 @@ public class DoubleArrayTrie implements Dictionary {
     private static final int  NONE            = 0;
 
     public static DoubleArrayTrie create(Iterable<String> words) {
-        DoubleArrayTrie trie = new DoubleArrayTrie(IntsRefWrapper.INSTANCE);
+        DoubleArrayTrie trie = new DoubleArrayTrie(DefaultAlphabetTransformer.INSTANCE);
         for (String word : words) {
             trie.add(word);
         }
@@ -214,7 +214,7 @@ public class DoubleArrayTrie implements Dictionary {
         return nextState(state, END_INPUT) != alphabetTransformer.unwrap(NO_SUCH_STATE);
     }
 
-    public enum IntsRefWrapper implements AlphabetTransformer {
+    public enum DefaultAlphabetTransformer implements AlphabetTransformer {
         INSTANCE;
         /**
          * 中文字符的起始数字
