512b89dca2b414540f5bf18c0b338ef013da021c
hang.gao <hang.gao@limiku.com>
2015-12-17 13:37:56 +0800

    字符串匹配,结合BF和KMP算法

--- src/main/java/cn/yxffcode/easytookit/utils/StringUtils.java
+++ src/main/java/cn/yxffcode/easytookit/utils/StringUtils.java
@@ -5,6 +5,7 @@ package cn.yxffcode.easytookit.utils;
  */
 public final class StringUtils {
   public static final String EMPTY = "";
+  private static final int STRING_INDEX_OF_THRESHOLD = 20;
 
   private StringUtils() {
   }
@@ -25,4 +26,164 @@ public final class StringUtils {
   public static boolean equalsIgnoreCase(String left, String right) {
     return (left == right) || (left != null && left.equalsIgnoreCase(right));
   }
+
+  public static int indexOf(String source, String target) {
+    return indexOf(source, 0, source.length(), target, 0, target.length());
+  }
+
+  /**
+   * @see #indexOf(char[], int, int, char[], int, int)
+   */
+  public static int indexOf(String source, int soff, int slen,
+                            String target, int toff, int tlen) {
+    if (slen < STRING_INDEX_OF_THRESHOLD) {
+      if (tlen == 0) {
+        return 0;
+      }
+
+      char first = target.charAt(toff);
+      int max = soff + (slen - tlen);
+
+      for (int i = soff; i <= max; i++) {
+        if (source.charAt(i) != first) {
+          while (++i <= max && source.charAt(i) != first) ;
+        }
+        if (i <= max) {
+          int j = i + 1;
+          int end = j + tlen - 1;
+          for (int k = toff + 1; j < end && source.charAt(j) == target.charAt(k); j++, k++) {
+            //do nothing
+          }
+
+          if (j == end) {
+            return i - soff;
+          }
+        }
+      }
+      return -1;
+    }
+    //find first equals character
+    char first = target.charAt(toff);
+    int max = soff + (slen - tlen);
+    int idx = soff;
+    while (idx <= max) {
+      if (source.charAt(idx) != first) {
+        while (++idx <= max && source.charAt(idx) != first) ;
+      }
+      if (idx > max) {
+        return -1;
+      }
+      break;
+    }
+    //KMP next array
+    int[] next = new int[tlen];
+    next[0] = -1;
+    int k = next[0];//可当作i==0的情况,k = next[i]
+    for (int i = 0, j = target.length() - 1; i < j; ) {
+      if (k == -1 || target.charAt(k + toff) == target.charAt(i + toff)) {
+        //next[i+1]=next[i]+1 => next[i+1]=k+1
+        next[++i] = ++k;
+      } else {
+        k = next[k];
+      }
+    }
+    //from idx not 0
+    int i = idx;
+    int j = 0;
+    while (i < slen && j < tlen) {
+      if (j == -1 || target.charAt(toff + j) == source.charAt(soff + i)) {
+        ++i;
+        ++j;
+      } else {
+        j = next[j];
+      }
+      if (tlen - j > slen - i) {
+        return -1;
+      }
+    }
+    return j == tlen ? i - j : -1;
+  }
+
+  public static int indexOf(char[] source, char[] target) {
+    return indexOf(source, 0, source.length, target, 0, target.length);
+  }
+
+  /**
+   * 模式匹配,{@link String#indexOf(char[], int, int, char[], int, int, int)}方法使用的是BF的方式,
+   * 在模式串比较短时效果比KMP好,当模式串太长后,KMP的优势比较明显
+   * <p/>
+   * 给一个中间值{@link #STRING_INDEX_OF_THRESHOLD},当模式串长度小于中间值时使用BF,大于中间值时使用KMP
+   *
+   * @return -1表示没找到
+   */
+  public static int indexOf(char[] source, int soff, int slen,
+                            char[] target, int toff, int tlen) {
+    if (slen < STRING_INDEX_OF_THRESHOLD) {
+      if (tlen == 0) {
+        return 0;
+      }
+
+      char first = target[toff];
+      int max = soff + (slen - tlen);
+
+      for (int i = soff; i <= max; i++) {
+        if (source[i] != first) {
+          while (++i <= max && source[i] != first) ;
+        }
+        if (i <= max) {
+          int j = i + 1;
+          int end = j + tlen - 1;
+          for (int k = toff + 1; j < end && source[j] == target[k]; j++, k++)
+            ;
+
+          if (j == end) {
+                    /* Found whole string. */
+            return i - soff;
+          }
+        }
+      }
+      return -1;
+    }
+    //find first equals character
+    char first = target[toff];
+    int max = soff + (slen - tlen);
+    int idx = soff;
+    while (idx <= max) {
+      if (source[idx] != first) {
+        while (++idx <= max && source[idx] != first) ;
+      }
+      if (idx > max) {
+        return -1;
+      }
+      break;
+    }
+    //KMP next array
+    int[] next = new int[tlen];
+    next[0] = -1;
+    int k = next[0];//可当作i==0的情况,k = next[i]
+    for (int i = 0, j = target.length - 1; i < j; ) {
+      if (k == -1 || target[k + toff] == target[i + toff]) {
+        //next[i+1]=next[i]+1 => next[i+1]=k+1
+        next[++i] = ++k;
+      } else {
+        k = next[k];
+      }
+    }
+    //from idx not 0
+    int i = idx;
+    int j = 0;
+    while (i < slen && j < tlen) {
+      if (j == -1 || target[toff + j] == source[soff + i]) {
+        ++i;
+        ++j;
+      } else {
+        j = next[j];
+      }
+      if (tlen - j > slen - i) {
+        return -1;
+      }
+    }
+    return j == tlen ? i - j : -1;
+  }
+
 }
