b0e2b4e475dfbc1180cd39ebcbe2b730ff6b8afc
hang.gao <hang.gao@limiku.com>
2015-12-18 12:12:18 +0800

    KMP

---
+++ src/main/java/cn/yxffcode/easytookit/algorithm/CharsequenceMatcher.java
@@ -0,0 +1,118 @@
+package cn.yxffcode.easytookit.algorithm;
+
+/**
+ * @author gaohang on 15/12/18.
+ * @see CharsMatcher
+ */
+public class CharsequenceMatcher {
+
+  private static final int STRING_INDEX_OF_THRESHOLD = 10;
+
+  public static CharsequenceMatcher create(CharSequence target, int toff, int tlen) {
+    return new CharsequenceMatcher(target, toff, tlen);
+  }
+
+  private int[] next;
+  private final CharSequence target;
+  private final int toff;
+  private final int tlen;
+
+  private CharsequenceMatcher(CharSequence target, int toff, int tlen) {
+    this.target = target;
+    this.toff = toff;
+    this.tlen = tlen;
+  }
+
+  public CharsequenceMatcher buildNextIfAbsent() {
+    if (next == null) {
+      synchronized (next) {
+        if (next == null) {
+          next = compileNext(target, toff, tlen);
+        }
+      }
+    }
+    return this;
+  }
+
+  /**
+   * 调用此方法前,如果没有调用{@link #buildNextIfAbsent()},会以创建next数组,但next数组可能会被创建多次
+   */
+  public int indexOf(CharSequence source, int soff, int slen) {
+    if (slen < STRING_INDEX_OF_THRESHOLD) {
+      if (tlen == 0) {
+        return 0;
+      }
+
+      char first = target.charAt(toff);
+      int max = soff + (slen - tlen);
+
+      for (int i = soff; i <= max; i++) {
+        if (source.charAt(i) != first) {
+          while (++i <= max && source.charAt(i) != first) ;
+        }
+        if (i <= max) {
+          int j = i + 1;
+          int end = j + tlen - 1;
+          for (int k = toff + 1; j < end && source.charAt(j) == target.charAt(k); j++, k++) {
+            //do nothing
+          }
+
+          if (j == end) {
+            return i - soff;
+          }
+        }
+      }
+      return -1;
+    }
+    //find first equals character
+    char first = target.charAt(toff);
+    int max = soff + (slen - tlen);
+    int idx = soff;
+    if (source.charAt(idx) != first) {
+      while (++idx <= max && source.charAt(idx) != first) ;
+    }
+    if (idx > max) {
+      return -1;
+    }
+    if (next == null) {
+      next = compileNext(target, toff, tlen);
+    }
+    //from idx not 0
+    int i = idx - soff;
+    int j = 0;
+    while (i < slen && j < tlen) {
+      if (j == 0 && target.charAt(toff + j) != source.charAt(soff + i)) {
+        while (++i <= max && source.charAt(i) != first) ;
+      }
+      if (i >= tlen) {
+        return -1;
+      }
+      if (j == -1 || target.charAt(toff + j) == source.charAt(soff + i)) {
+        ++i;
+        ++j;
+      } else {
+        j = next[j];
+      }
+      if (tlen - j > slen - i) {
+        return -1;
+      }
+    }
+    return j == tlen ? i - j : -1;
+  }
+
+  private int[] compileNext(CharSequence target, int toff, int tlen) {
+    int[] next = new int[tlen];
+    next[0] = -1;
+    int k = next[0];//可当作i==0的情况,k = next[i]
+    for (int i = 0, j = target.length() - 1; i < j; ) {
+      if (k == -1 || target.charAt(k + toff) == target.charAt(i + toff)) {
+        //next[i+1]=next[i]+1 => next[i+1]=k+1
+        next[++i] = ++k;
+      } else {
+        k = next[k];
+      }
+    }
+    return next;
+  }
+
+}
--- src/main/java/cn/yxffcode/easytookit/utils/StringUtils.java
+++ src/main/java/cn/yxffcode/easytookit/utils/StringUtils.java
@@ -1,6 +1,7 @@
 package cn.yxffcode.easytookit.utils;
 
 import cn.yxffcode.easytookit.algorithm.CharsMatcher;
+import cn.yxffcode.easytookit.algorithm.CharsequenceMatcher;
 import cn.yxffcode.easytookit.dic.DoubleArrayTrie;
 import cn.yxffcode.easytookit.participle.DictionaryTokenFilter;
 
@@ -40,75 +41,7 @@ public final class StringUtils {
    */
   public static int indexOf(CharSequence source, int soff, int slen,
                             CharSequence target, int toff, int tlen) {
-    if (slen < STRING_INDEX_OF_THRESHOLD) {
-      if (tlen == 0) {
-        return 0;
-      }
-
-      char first = target.charAt(toff);
-      int max = soff + (slen - tlen);
-
-      for (int i = soff; i <= max; i++) {
-        if (source.charAt(i) != first) {
-          while (++i <= max && source.charAt(i) != first) ;
-        }
-        if (i <= max) {
-          int j = i + 1;
-          int end = j + tlen - 1;
-          for (int k = toff + 1; j < end && source.charAt(j) == target.charAt(k); j++, k++) {
-            //do nothing
-          }
-
-          if (j == end) {
-            return i - soff;
-          }
-        }
-      }
-      return -1;
-    }
-    //find first equals character
-    char first = target.charAt(toff);
-    int max = soff + (slen - tlen);
-    int idx = soff;
-    if (source.charAt(idx) != first) {
-      while (++idx <= max && source.charAt(idx) != first) ;
-    }
-    if (idx > max) {
-      return -1;
-    }
-    //KMP next array
-    int[] next = new int[tlen];
-    next[0] = -1;
-    int k = next[0];//可当作i==0的情况,k = next[i]
-    for (int i = 0, j = target.length() - 1; i < j; ) {
-      if (k == -1 || target.charAt(k + toff) == target.charAt(i + toff)) {
-        //next[i+1]=next[i]+1 => next[i+1]=k+1
-        next[++i] = ++k;
-      } else {
-        k = next[k];
-      }
-    }
-    //from idx not 0
-    int i = idx - soff;
-    int j = 0;
-    while (i < slen && j < tlen) {
-      if (j == 0 && target.charAt(toff + j) != source.charAt(soff + i)) {
-        while (++i <= max && source.charAt(i) != first) ;
-      }
-      if (i >= tlen) {
-        return -1;
-      }
-      if (j == -1 || target.charAt(toff + j) == source.charAt(soff + i)) {
-        ++i;
-        ++j;
-      } else {
-        j = next[j];
-      }
-      if (tlen - j > slen - i) {
-        return -1;
-      }
-    }
-    return j == tlen ? i - j : -1;
+    return CharsequenceMatcher.create(target, toff, tlen).indexOf(source, soff, slen);
   }
 
   public static int indexOf(char[] source, char[] target) {
