3de04d52a011417f6bc30fc19d4a20846021fbf1
hang.gao <hang.gao@limiku.com>
2016-07-29 11:10:41 +0800

    doc

---
+++ src/test/java/cn/yxffcode/freetookit/lang/FluentOptionalTest.java
@@ -0,0 +1,13 @@
+package cn.yxffcode.freetookit.lang;
+
+import org.junit.Test;
+
+/**
+ * @author gaohang on 16/7/29.
+ */
+public class FluentOptionalTest {
+
+    @Test
+    public void test() {
+    }
+}
